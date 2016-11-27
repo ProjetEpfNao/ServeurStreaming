e23600da6f1030648fa0c4c9692ee6733999d845
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-11-27 06:11:55 +0100

    Added flask middleman app for file transfer from nao

---
+++ roles/streaming_server/templates/flask_app.py
@@ -0,0 +1,56 @@
+import os
+import uuid
+import time
+import threading
+import sys
+from flask import Flask, request
+
+FFSERVER_URL = "http://54.84.19.127:8090/feed1.ffm"
+LENGTH = 4
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+
+file_counter = 10
+
+def stream_file(path):
+    print(path)
+    os.system("ffmpeg -i " + path + " " + FFSERVER_URL)
+
+def run_stream():
+    while 1:
+        paths = os.listdir(app.config['UPLOAD_FOLDER'])
+        if len(paths) == 0:
+            time.sleep(0.1)
+            continue
+        print(paths)
+        if len(paths) > 10:
+            for path in paths[:-1]:
+                os.remove(os.path.join(app.config['UPLOAD_FOLDER'],path))
+            paths[:] = [paths[-1]]
+        path = os.path.join(app.config['UPLOAD_FOLDER'],paths.pop(0))
+        stream_file(path)
+        try:
+            os.remove(path)
+        except:
+            #print(path)
+            #print(sys.exc_info())
+            pass
+
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    global file_counter
+    file = request.files['file']
+    filename = str(file_counter) + ".mp4"
+    file_counter += 1
+    path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(path)
+    return "1"
+
+if __name__ == '__main__':
+    threading.Thread(target=run_stream).start()
+    app.run(
+        host="0.0.0.0",
+        port=int("80"),
+        debug=True
+    )

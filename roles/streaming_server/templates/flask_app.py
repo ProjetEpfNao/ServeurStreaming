f2126cd35f91fb4dc4fb241fb75791cd1daec530
EtienneDesticourt <etienne.desticourt@gmail.com>
2016-11-29 18:05:07 +0100

    Added logging to middleman, fixed warnings

--- roles/streaming_server/templates/flask_app.py
+++ roles/streaming_server/templates/flask_app.py
@@ -4,16 +4,22 @@ import time
 import threading
 import sys
 from flask import Flask, request
+import logging
 
 FFSERVER_URL = "http://localhost:8090/feed1.ffm"
 LENGTH = 4
+
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = '/home/ubuntu/flask_app/uploads'
+logging.basicConfig(format='%(asctime)s %(message)s', 
+                    datefmt='%m/%d/%Y %I:%M:%S %p',
+                    filename='middleman.log',
+                    level=logging.DEBUG)
 
 file_counter = 10
 
 def stream_file(path):
-    print(path)
+    #print(path)
     os.system("ffmpeg -i " + path + " " + FFSERVER_URL)
 
 def run_stream():
@@ -22,16 +28,18 @@ def run_stream():
         if len(paths) == 0:
             time.sleep(0.1)
             continue
-        print(paths)
+        #print(paths)
         if len(paths) > 10:
             for path in paths[:-1]:
                 os.remove(os.path.join(app.config['UPLOAD_FOLDER'],path))
             paths[:] = [paths[-1]]
+            logging.info('Max delay reached. Batch delete executed.')
         path = os.path.join(app.config['UPLOAD_FOLDER'],paths.pop(0))
         stream_file(path)
         try:
             os.remove(path)
         except:
+            logging.error('Failed to delete file: '+path)
             #print(path)
             #print(sys.exc_info())
             pass
@@ -45,6 +53,7 @@ def upload():
     file_counter += 1
     path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     file.save(path)
+    logging.info('Received and save file to '+filename+' from '+str(request.remote_addr))
     return "1"
 
 if __name__ == '__main__':
